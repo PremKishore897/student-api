d6047567d0cbcdc3c816248285c4dc239a0e49cf
PremKishore897 <premkishore897@gmail.com>
2024-12-01 15:19:10 +0530

    Updated database.py

--- database.py
+++ database.py
@@ -1,10 +1,5 @@
 from pymongo.mongo_client import MongoClient
-from dotenv import load_dotenv
-import sys
 import os
-
-print(sys.argv)
-load_dotenv("secrets.env")
 
 URI = os.getenv("DATABASE_URI")
 # print(URI)
